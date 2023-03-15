64b72c6c6d05532bd70f3410d2dfd568b0c85922
AlexBurg <alex.burkh@yandex.ru>
2023-03-15 15:01:57 +0300

    Реализовано консольное логгирование через интерфейс

--- lib/support/ConsoleLogger.java
+++ lib/support/ConsoleLogger.java
@@ -1,5 +1,25 @@
 package lib.support;
 
-public class ConsoleLogger implements Logger{
-    
+import java.time.LocalDateTime;
+
+public class ConsoleLogger implements Logger {
+    @Override
+    public void INFO(String textToLog) {
+        System.out.println(" ? [" + LocalDateTime.now() + "] INFO: " + textToLog + "\n");
+    }
+
+    @Override
+    public void WARNING(String textToLog) {
+        System.out.println(" * [" + LocalDateTime.now() + "] WARNING: " + textToLog + "\n");
+    }
+
+    @Override
+    public void ERROR(String textToLog) {
+        System.out.println(" ! [" + LocalDateTime.now() + "] ERROR: " + textToLog + "\n");
+    }
+
+    @Override
+    public void CRYTICAL(String textToLog) {
+        System.out.println(" X [" + LocalDateTime.now() + "] CRYTICAL: " + textToLog + "\n");
+    }
 }

abef7f71316d630ebf17063f55b9b00a672b1cbd
AlexBurg <alex.burkh@yandex.ru>
2023-03-15 15:32:29 +0300

    Основа класса Toy

--- lib/dataModels/Toy.java
+++ lib/dataModels/Toy.java
@@ -1,5 +1,33 @@
 package lib.dataModels;
 
 public class Toy {
-    
+    private int _id;
+    private String _productName;
+    private double _winChance;
+
+    public Toy(int id, String productName, double winChance) {
+        this._id = id;
+        this._productName = productName;
+        this._winChance = winChance;
+    }
+
+    @Override
+    public String toString() {
+        return "Toy {id=" + _id + ", productName=" + _productName + ", chanceWeight=" + _winChance + "}";
+    }
+
+    /* GETTERS */
+    public int get_id() {
+        return _id;
+    }
+    public String get_productName() {
+        return _productName;
+    }
+    public double get_chanceWeight() {
+        return _winChance;
+    }
+
+    public void changeWinChance(double chance) {
+        this._winChance = chance;
+    }
 }

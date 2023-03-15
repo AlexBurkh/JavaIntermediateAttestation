300f262d46034d8bfb8a9dea61f3436a3e61fa8b
AlexBurg <alex.burkh@yandex.ru>
2023-03-15 12:52:39 +0300

    Созданы и оттестированы классы консольного UI

--- lib/ui/ConsoleView.java
+++ lib/ui/ConsoleView.java
@@ -1,5 +1,46 @@
 package lib.ui;
 
+import java.util.Scanner;
+
 public class ConsoleView implements View{
-    
+    Scanner sc;
+
+    public ConsoleView() {
+        this.sc = new Scanner(System.in);
+    }
+
+    @Override
+    public int readInt(String text) {
+        print(text);
+        return readInt();
+    }
+
+    @Override
+    public int readInt() {
+        return sc.nextInt();
+    }
+
+    @Override
+    public String readText(String text) {
+        print(text);
+        return readText();
+    }
+
+    @Override
+    public String readText() {
+        return sc.nextLine();
+    }
+
+    public void print(String text) {
+        System.out.print(text);
+    }
+    @Override
+    public void println(String text) {
+        System.out.println(text);
+    }
+
+    @Override
+    public void println(int num) {
+        System.out.println(num);
+    }
 }
--- lib/ui/View.java
+++ lib/ui/View.java
@@ -1,5 +1,15 @@
 package lib.ui;
 
 public interface View {
+    public int readInt(String text);
 
+    public int readInt();
+
+    public String readText(String text);
+
+    public String readText();
+
+    public void print(String text);
+    public void println(String text);
+    public void println(int num);
 }

e3aa070fd89db5883363bb54f2d1c4a76d00e185
AlexBurg <alex.burkh@yandex.ru>
2023-03-15 16:59:51 +0300

    Реализована первая версия roll-а, добавление игрушки

--- lib/dataModels/Toy.java
+++ lib/dataModels/Toy.java
@@ -4,30 +4,42 @@ public class Toy {
     private int _id;
     private String _productName;
     private double _winChance;
+    private int _amount;
 
-    public Toy(int id, String productName, double winChance) {
+    public Toy(int id, String productName, double winChance, int amount) {
         this._id = id;
         this._productName = productName;
         this._winChance = winChance;
+        this._amount = amount;
     }
 
     @Override
     public String toString() {
-        return "Toy {id=" + _id + ", productName=" + _productName + ", chanceWeight=" + _winChance + "}";
+        return "Toy {id: " + _id + ", productName: " + _productName + ", chanceWeight: " + _winChance + ", amount: " + _amount +"}";
     }
 
     /* GETTERS */
-    public int get_id() {
+    public int getId() {
         return _id;
     }
-    public String get_productName() {
+    public String getProductName() {
         return _productName;
     }
-    public double get_chanceWeight() {
+    public double getWinChance() {
         return _winChance;
+    }
+    public int getAmount() {
+        return _amount;
     }
 
     public void changeWinChance(double chance) {
         this._winChance = chance;
+    }
+    public Toy take() {
+        this._amount -= 1;
+        return this;
+    }
+    public void put() {
+        this._amount += 1;
     }
 }
--- lib/dataModels/ToyMachine.java
+++ lib/dataModels/ToyMachine.java
@@ -1,10 +1,68 @@
 package lib.dataModels;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lib.support.Logger;
 import lib.ui.View;
 
 public class ToyMachine {
     private Logger _logger;
     private View _view;
+    private int _next_id = 0;
+
+    private List<Toy> _playable;
+    private List<Toy> _won;
+
+    public ToyMachine(Logger _logger, View _view, List<Toy> playable, List<Toy> won) {
+        this._logger = _logger;
+        this._view = _view;
+        this._playable = playable;
+        this._won = won;
+    }
+
+    public ToyMachine(Logger logger, View view) {
+        this(logger, view, new ArrayList<>(), new ArrayList<>());
+    }
+
+    public void roll() {
+        List<Double> weights = new ArrayList<Double>();
+        double sum = 0;
+        for (Toy t : _playable) {
+            sum += t.getWinChance();
+            weights.add(sum);
+        }
+        _logger.INFO("сформирована функция распределения вероятностей");
+        double userRoll = Math.random() * sum;
+        _logger.INFO("выброшено число " + userRoll);
+        int toyIndex = -1;
+        for (int i = 0; i < weights.size(); i++) {
+            if (i == 0) {
+                if (userRoll >= 0 && userRoll < weights.get(i)) {
+                    toyIndex = 0;
+                    _logger.INFO("выпала игрушка № 1");
+                }
+            }
+            else {
+                if (userRoll >= weights.get(i - 1) && userRoll < weights.get(i)) {
+                    toyIndex = i;
+                    _logger.INFO("выпала игрушка № " + (toyIndex + 1));
+                }
+            }
+        }
+        if (toyIndex >= 0) {
+            Toy t = _playable.get(toyIndex).take();
+            _won.add(t);
+            _logger.INFO("имя игрушки: " + t.getProductName());
+            _logger.INFO("игрушка перемещена в список выигранных");
+        }
+        else {
+            _logger.ERROR("ошибка работы механизма выбора игрушки по броску");
+        }
+    }
     
+    public void addToy(String name, double winChance, int amount) {
+        _playable.add(new Toy(this._next_id, name, winChance, amount));
+        _next_id += 1;
+    }
 }

a65d5cd74516328a92b8f305d345d08f0f25d4fc
alex <alex.burkh@yandex.ru>
2023-03-16 17:18:29 +0300

    добавлен главнный класс программы

---
+++ Program.java
@@ -0,0 +1,27 @@
+import lib.dataModels.Toy;
+import lib.dataModels.ToyMachine;
+import lib.support.Logger;
+import lib.support.ConsoleLogger;
+import lib.ui.View;
+import lib.ui.ConsoleView;
+
+class Program {
+    static Logger _logger;
+    static View _view;
+    static ToyMachine _tm;
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    private static void run() {
+        init();
+        _view.print("Привет");
+    }
+
+    private static void init() {
+        _logger = new ConsoleLogger();
+        _view = new ConsoleView();
+        _tm = new ToyMachine(_logger);
+    }
+}
